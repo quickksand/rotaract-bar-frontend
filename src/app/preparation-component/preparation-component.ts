93c46123a56e9c97ec6233fff3efe78185c74aef
Leon Lichius <leon.lichius@mailbox.org>
2026-06-06 14:53:50 +0200

    back to order screen when last order in prep screen is finished

--- src/app/preparation-component/preparation-component.ts
+++ src/app/preparation-component/preparation-component.ts
@@ -1,6 +1,9 @@
 // preparation.component.ts
-import {Component, inject} from '@angular/core';
+import {Component, DestroyRef, inject, OnInit} from '@angular/core';
 import {AsyncPipe, CurrencyPipe} from '@angular/common';
+import {Router} from '@angular/router';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {skip} from 'rxjs';
 import {OrderService} from '../services/order.service';
 
 @Component({
@@ -12,12 +15,25 @@ import {OrderService} from '../services/order.service';
   ],
   styleUrls: ['./preparation-component.css']
 })
-export class PreparationComponent {
+export class PreparationComponent implements OnInit {
 
   protected orderService = inject(OrderService);
+  private router = inject(Router);
+  private destroyRef = inject(DestroyRef);
 
   get activeOrders() {
     return this.orderService.activePreparationOrders$;
+  }
+
+  ngOnInit() {
+    this.orderService.activePreparationOrders$.pipe(
+      skip(1),
+      takeUntilDestroyed(this.destroyRef)
+    ).subscribe(orders => {
+      if (orders.length === 0) {
+        this.router.navigate(['/order']);
+      }
+    });
   }
 
   startOrder(orderId: number) {
